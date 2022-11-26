37295c78ac01d8f3f4142a58cda1641ae286687c
mmlbpdb <mmlbpdb@skyline.be>
2022-11-26 18:18:34 +0100

    test

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+	public class Utility
+	{
+		public static string ToUpper(string input)
+		{
+			return input.ToUpper();
+		}
+	}
+}
---
+++ ClassLibraryTests/UtilityTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Tests
+{
+	[TestClass()]
+	public class UtilityTests
+	{
+		[TestMethod()]
+		public void ToUpperTest()
+		{
+			var result = Utility.ToUpper("a");
+			Assert.AreEqual("A", result);
+		}
+	}
+}
